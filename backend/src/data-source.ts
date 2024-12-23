54d48554408ad1c8e8d31ee1cd7748dd3ee26326
jiashanlu <benjaminbois@gmail.com>
2024-12-23 12:25:37 +0000

    eee

--- backend/src/data-source.ts
+++ backend/src/data-source.ts
@@ -9,5 +9,7 @@ export const AppDataSource = new DataSource({
   url: process.env.DATABASE_URL,
   entities: [Price],
   migrations: [__dirname + "/migrations/*.{js,ts}"],
-  ssl: false,
+  ssl: {
+    rejectUnauthorized: true,
+  },
 });
